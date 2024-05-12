0487697a0f1210b48dff0785f18966b3d560e847
harshit gupta <harshit.gupta2@olacabs.com>
2024-05-12 21:54:26 +0530

    added chat memory

--- backend/core.py
+++ backend/core.py
@@ -5,8 +5,8 @@ load_dotenv()
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 import os
-from typing import Any
-from langchain.chains import RetrievalQA
+from typing import Any, Dict, List
+from langchain.chains import ConversationalRetrievalChain
 from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
 from pinecone import Pinecone
 from consts import INDEX_NAME
@@ -14,19 +14,17 @@ from consts import INDEX_NAME
 pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = PineconeLangChain.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
